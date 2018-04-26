59560c3e5e4522e06bb940a1d282b347b601caea
Unknown <maxime.ferraille@gmail.com>
2018-04-26 15:40:32 +0200

    Create UIColor extension

---
+++ attendance-assignment/Extension/UIColor+MainColor.swift
@@ -0,0 +1,20 @@
+//
+//  UIColor+MainColor.swift
+//  attendance-assignment
+//
+//  Created by Maxime Ferraille on 26/04/2018.
+//  Copyright © 2018 Maxime Ferraille. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor{
+    struct MainColor {
+// Example
+//        struct Red {
+//            static let Firebrick = UIColor(red: 0.3333, green: 0.01, blue: 0.1, alpha: 0.7)
+//            static let Tomato = UIColor(red: 0.255, green: 0.99, blue: 0.71, alpha: 1)
+//        }
+    }
+}
