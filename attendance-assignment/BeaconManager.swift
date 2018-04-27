a7401e0306268a5c84cffd39f387e29c0be6c0b8
Unknown <maxime.ferraille@gmail.com>
2018-04-27 16:12:28 +0200

    Add beacon Manager

---
+++ attendance-assignment/BeaconManager.swift
@@ -0,0 +1,47 @@
+//
+//  BeaconManager.swift
+//  attendance-assignment
+//
+//  Created by Maxime Ferraille on 27/04/2018.
+//  Copyright © 2018 Maxime Ferraille. All rights reserved.
+//
+
+import Foundation
+import CoreLocation
+
+let sharedBeaconManager = BeaconManager()
+
+class BeaconManager : NSObject, CLLocationManagerDelegate {
+    let manager = CLLocationManager()
+    let proximityUUID = UUID(uuidString: "F2A74FC4-7625-44DB-9B08-CB7E130B2029")
+    let beaconID = "com.attendanceAssignment.BeaconRegion"
+    
+    override init() {
+        super.init()
+        manager.delegate = self
+    }
+    
+    func requestAuthorization(){
+        manager.requestWhenInUseAuthorization()
+        
+        let status = CLLocationManager.authorizationStatus()
+        
+        if status != .authorizedWhenInUse || status != .authorizedWhenInUse {
+            manager.requestWhenInUseAuthorization()
+        }
+    }
+    
+    func monitorBeacons() {
+        if CLLocationManager.isMonitoringAvailable(for: CLBeaconRegion.self) {
+            let beaconRegion = CLBeaconRegion(proximityUUID: proximityUUID!, identifier: beaconID)
+            print(beaconRegion)
+            self.manager.startMonitoring(for: beaconRegion)
+        }
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        if status == .authorizedAlways || status == .authorizedWhenInUse {
+            monitorBeacons()
+        }
+    }
+}
